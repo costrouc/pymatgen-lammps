682c65404eaabbb60ce0b91a04e69597b75a2a3f
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2017-08-17 15:17:07 -0400

    bump version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-version = '0.0.5'
+version = '0.0.6'
 setup(
     name='pymatgen-lammps',
     version=version,
